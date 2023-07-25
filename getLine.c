e9eb9fdbfa58954dc2fecf42a303416eed9ffd39
axe <78966503+GOLVEN666@users.noreply.github.com>
2023-07-25 22:01:10 +0100

    Add files via upload

--- getLine.c
+++ getLine.c
@@ -46,40 +46,42 @@ return (r);
  * @info: parameter struct
  * Return: bytes read
  */
-
 ssize_t get_input(info_t *info)
 {
-static char *buf; /* the ';' command chain buffer */
-static size_t i, j, len;
-ssize_t r = 0;
-char **buf_p = &(info->arg), *p;
-_putchar(BUF_FLUSH);
-r = input_buf(info, &buf, &len);
-if (r == -1) /* EOF */
-return (-1);
-if (len) /* we have commands left in the chain buffer */
-{
-j = i; /* init new iterator to current buf position */
-p = buf + i; /* get pointer for return */
-check_chain(info, buf, &j, i, len);
-while (j < len) /* iterate to semicolon or end */
-{
-if (is_chain(info, buf, &j))
-break;
-j++;
-}
-i = j + 1; /* increment past nulled ';'' */
-if (i >= len) /* reached end of buffer? */
-{
-i = len = 0; /* reset position and length */
-info->cmd_buf_type = CMD_NORM;														}
-*buf_p = p; /* pass back pointer to current command position */
-return (_strlen(p)); /* return length of current command */
-}
-*buf_p = buf; /* else not a chain, pass back buffer from _getline() */
-return (r); /* return length of buffer from _getline() */
-}
+    static char *buf = NULL; /* the ';' command chain buffer */
+    static size_t i = 0, len = 0;
+    ssize_t r = 0;
+    char **buf_p = &(info->arg), *p;
 
+    _putchar(BUF_FLUSH);
+    r = input_buf(info, &buf, &len);
+    if (r == -1) /* EOF */
+        return (-1);
+
+    if (len) /* we have commands left in the chain buffer */
+    {
+        size_t j = i; /* init new iterator to current buf position */
+        p = buf + i; /* get pointer for return */
+        check_chain(info, buf, &j, i, len);
+        while (j < len) /* iterate to semicolon or end */
+        {
+            if (is_chain(info, buf, &j))
+                break;
+            j++;
+        }
+        i = j + 1; /* increment past nulled ';'' */
+        if (i >= len) /* reached end of buffer? */
+        {
+            i = len = 0; /* reset position and length */
+            info->cmd_buf_type = CMD_NORM;
+        }
+        *buf_p = p; /* pass back pointer to current command position */
+        return (_strlen(p)); /* return length of current command */
+    }
+
+    *buf_p = buf; /* else not a chain, pass back buffer from _getline() */
+    return (r); /* return length of buffer from _getline() */
+}
 /**
  * read_buf - reads a buffer
  * @info: parameter struct
